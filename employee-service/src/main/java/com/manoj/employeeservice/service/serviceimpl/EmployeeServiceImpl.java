da684ffaa784ea13ee45d86680644e862b7bf25d
Manoj H C <manojhc110@gmail.com>
2025-09-29 01:41:01 +0530

    circuit breaker done, retry done

--- employee-service/src/main/java/com/manoj/employeeservice/service/serviceimpl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/manoj/employeeservice/service/serviceimpl/EmployeeServiceImpl.java
@@ -8,8 +8,12 @@ import com.manoj.employeeservice.feignclient.APIClient;
 import com.manoj.employeeservice.mapper.EmployeeMapper;
 import com.manoj.employeeservice.repository.EmployeeRepository;
 import com.manoj.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +22,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private static Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     //private final RestTemplate restTemplate;
 
@@ -34,9 +39,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return savedEmployeeDto;
     }
-
+    //@CircuitBreaker(name = "getEmployeeByIdCB", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "getEmployeeByIdCB", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        logger.info("inside getEmployeeById() method");
         Employee employee = employeeRepository.findById(employeeId).get();
 
         //rest template call for getting department details of the employee
@@ -52,6 +59,29 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //using feign client
        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+        //setting all values into one dto
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+        return apiResponseDto;
+    }
+
+    //fallback method must have the same return type and parameter as circuit breaker
+    /*
+java.lang.NoSuchMethodException: class com.manoj.employeeservice.dto.APIResponseDto class com.manoj.employeeservice.service
+.serviceimpl.EmployeeServiceImpl.getDefaultDepartment(class java.lang.Long,class java.lang.Throwable)*/
+//    to resolve the above error i need to add Exception as a parameter
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception){
+        logger.info("inside getDefaultDepartment() method");
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Department");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Department");
 
         EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
         //setting all values into one dto
